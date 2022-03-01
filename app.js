20e0a45fc586787678adc9a10c2eec4b4439c424
emre-guler <emre_guler01@hotmail.com>
2022-03-01 22:59:26 +0300

    npm packages and first run

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
